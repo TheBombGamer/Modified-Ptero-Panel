99b0dba1f81bb3af3a03e95d316a4383a8f51388
Kyle Speight <Electr_stream@outlook.com>
2025-03-01 13:24:53 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ sudo apt install python3 python3-pip curl websockify git
 mkdir flutter-panel && cd flutter-panel
 git pull https://github.com/TheBombGamer/flutter-panel.git
 pip install -r requirements.txt
+
