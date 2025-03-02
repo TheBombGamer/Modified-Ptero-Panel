4c9ba5835b8b455d2684c46233b86ee14874f769
TheBombGamer <electr_stream@outlook.com>
2025-03-02 18:44:28 -0500

    123

--- web/index.php
+++ web/index.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
     <html lang="en">
-<?php 
 
-?>
     <head>
         <link rel="stylesheet" href="css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php $PRINT = 'DIDDY';echo($PRINT);?></title>
+        <title></title>
     </head>
     <body> 
-        <?php ?> 
-        <script src="src.js"></script>
+        <script type='text/javascript' src='//pl26009732.effectiveratecpm.com/ba/c4/31/bac4314a2fed49a691e3415a519eeb4c.js'></script>
     </body>
     </html>
