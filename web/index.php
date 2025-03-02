a971c03b509ee08c50c9b2a1c8f8fcb34338d733
Kyle Speight <Electr_stream@outlook.com>
2025-03-01 22:31:47 -0800

    php :3

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=7">
+    <meta name="keywords" content="Kyle Hosting, Surge, Surge Hosting, Surge Panel, Panel, Surge Hosting Panel, Hosting Panel, Lightweight Panel, Lightweight Hosting, Tech, Wordpress, Docker, Github, Integrations, SSO">
+    <title>Document</title>
+</head>
+<body>
+    <?php print="test" ?>
+</body>
+</html>
